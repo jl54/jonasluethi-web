7e6f67e7c00da78d64d3ce9a230ce6e74879236d
jl54 <jonas@jonasluethi.dev>
2026-02-06 23:06:45 +0100

    feat(IaC): Adds opentofu config for hetzner infrastructure

---
+++ infra/main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    hcloud = {
+      source  = "hetznercloud/hcloud"
+      version = "~> 1.60.0"
+    }
+  }
+
+  backend "s3" {
+    key    = "prod/terraform.tfstate"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    use_path_style             = true
+  }
+}
+
+provider "hcloud" {
+  token = var.hcloud_token
+}
+
+resource "hcloud_server" "jl-web-01" {
+  name        = "jl-web-01"
+  image       = "ubuntu-24.04"
+  server_type = "cx23"
+  public_net {
+    ipv4_enabled = true
+    ipv6_enabled = true
+  }
+}
